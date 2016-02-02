133790805afb17903958d5504d041fdf05bb5c78
christian_hellebostad <christian_hellebostad@msn.com>
2016-02-02 12:53:38 +0100

    Kamera tråd v1.0

--- KrabbeROV-Embedded/KrabbeROV-Embedded/main.cpp
+++ KrabbeROV-Embedded/KrabbeROV-Embedded/main.cpp
@@ -1,1 +1,28 @@
-//test
+#include <iostream>
+#include <opencv2\highgui.hpp>
+#include <thread>
+
+
+using namespace std;
+using namespace cv;
+
+void cameraCapture() {
+	VideoCapture cap;
+	if (!cap.open(0))
+		cout << "Camera not found";
+	for (;;)
+	{
+		Mat frame;
+		cap >> frame;
+		imshow("this is you, smile! :)", frame);
+		if (waitKey(1) == 27) break; // stop capturing by pressing ESC 
+	}
+}
+
+int main(int argc, char** argv)
+{
+	
+	thread t1(cameraCapture);
+	t1.join();
+	return 0;
+}
